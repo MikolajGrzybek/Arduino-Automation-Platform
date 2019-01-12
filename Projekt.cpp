33c0c023a3d4d8540728900f7511520d24bcb4e7
MikolajGrzybek <grzybek.mikolaj@outlook.com>
2019-01-12 19:22:17 +0100

    Update Projekt.cpp

--- Projekt.cpp
+++ Projekt.cpp
@@ -23,7 +23,7 @@ SoftwareSerial mySerial(1, 0); // RX, TX
 
 //czujniki cyfrowe S1...S4 wpięte do wejść cryfowycj arduino nr.:2, 11...13
     const int DHT11_PIN=2; //Czujnik temperatury i wilgotności DHT11
-    const int S2=11;
+    const int Buzz=11; //Buzzer
     const int Trig=12; //Czujnik ultradźwiękowy HC-SR04 pin trig
     const int Echo=13; //Czujnik ultradźwiękowy HC-SR04 pin echo
     
@@ -34,6 +34,27 @@ SoftwareSerial mySerial(1, 0); // RX, TX
     const int SA4=A3;
     const int SA5=A4;
     const int SA6=A5;
+
+//zdefiniowanie zmiennych do piosenki granej prez Buzzer
+    const int c = 261;
+    const int d = 294;
+    const int e = 329;
+    const int f = 349;
+    const int g = 391;
+    const int gS = 415;
+    const int a = 440;
+    const int aS = 455;
+    const int b = 466;
+    const int cH = 523;
+    const int cSH = 554;
+    const int dH = 587;
+    const int dSH = 622;
+    const int eH = 659;
+    const int fH = 698;
+    const int fSH = 740;
+    const int gH = 784;
+    const int gSH = 830;
+    const int aH = 880;
     
 //zdefiniowanie zmiennej 'light_intensity', w której przetrzymywana jest informacja o natężeniu światła
     int light_intensity=0;
@@ -60,8 +81,9 @@ void setup() {
   pinMode(K6, OUTPUT);
   pinMode(K7, OUTPUT);
   pinMode(K8, OUTPUT);
-  
- 
+
+  pinMode(Buzz, OUTPUT);
+
   Serial1.begin(9600);
 
   dht.setup(DHT11_PIN);
@@ -69,7 +91,8 @@ void setup() {
 
 
 void loop() {
-
+    
+    send();
     pomiary();
 
     if(sterowanie = true)
@@ -80,13 +103,20 @@ void loop() {
     {
         reczne();
     }
- 
- 
   
+
   //For debugging purpose
   //Serial1.println(state);
 }
 
+void send()
+{
+    Serial1.println(light_intensity);
+    Serial1.println(wilgotnosc);
+    Serial1.println(temperatura);
+    Serial1.println(distance);
+
+}
 void pomiary()
 {
     //Dokonanie odczytu z czujników, zapis wartości do zmiennych i włączenie odpowiednich cewek
@@ -154,3 +184,116 @@ void reczne()
     default: break;
   }
 }
+
+/*void beep(int note, int duration)
+{
+  //Play tone on buzzerPin
+  tone(buzzerPin, note, duration);
+ 
+  //Play different LED depending on value of 'counter'
+  if(counter % 2 == 0)
+  {
+    digitalWrite(ledPin1, HIGH);
+    delay(duration);
+    digitalWrite(ledPin1, LOW);
+  }else
+  {
+    digitalWrite(ledPin2, HIGH);
+    delay(duration);
+    digitalWrite(ledPin2, LOW);
+  }
+ 
+  //Stop tone on buzzerPin
+  noTone(buzzerPin);
+ 
+  delay(50);
+ 
+  //Increment counter
+  counter++;
+}
+
+void firstSection()
+{
+  beep(a, 500);
+  beep(a, 500);    
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);  
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+ 
+  delay(500);
+ 
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(eH, 500);  
+  beep(fH, 350);
+  beep(cH, 150);
+  beep(gS, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+ 
+  delay(500);
+}
+ 
+void secondSection()
+{
+  beep(aH, 500);
+  beep(a, 300);
+  beep(a, 150);
+  beep(aH, 500);
+  beep(gSH, 325);
+  beep(gH, 175);
+  beep(fSH, 125);
+  beep(fH, 125);    
+  beep(fSH, 250);
+ 
+  delay(325);
+ 
+  beep(aS, 250);
+  beep(dSH, 500);
+  beep(dH, 325);  
+  beep(cSH, 175);  
+  beep(cH, 125);  
+  beep(b, 125);  
+  beep(cH, 250);  
+ 
+  delay(350);
+}
+  
+  //Play first section
+  //firstSection();
+ 
+  //Play second section
+ // secondSection();
+ 
+  //Variant 1
+  //beep(f, 250);  
+  beep(gS, 500);  
+  beep(f, 350);  
+  beep(a, 125);
+  beep(cH, 500);
+  beep(a, 375);  
+  beep(cH, 125);
+  beep(eH, 650);
+ 
+  delay(500);
+ 
+  //Repeat second section
+  secondSection();
+ 
+  //Variant 2
+  beep(f, 250);  
+  beep(gS, 500);  
+  beep(f, 375);  
+  beep(cH, 125);
+  beep(a, 500);  
+  beep(f, 375);  
+  beep(cH, 125);
+  beep(a, 650);  
+ 
+  delay(650);
+  */
