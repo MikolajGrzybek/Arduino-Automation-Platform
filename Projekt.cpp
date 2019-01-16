848d9942ba63f2765c7b3ef13d8b430822b6fa1b
MikolajGrzybek <grzybek.mikolaj@outlook.com>
2019-01-16 15:40:34 +0100

    Wstępna automatyka

--- Projekt.cpp
+++ Projekt.cpp
@@ -13,7 +13,7 @@ SoftwareSerial mySerial(1, 0); // RX, TX
 
 //cewka K1...K8, której sterowanie wpięte jest do wyjścia cyfrowego arduino nr.: 3...10
     const int K1=3; //lampa
-    const int K2=4;
+    const int K2=4; //brama
     const int K3=5;
     const int K4=6;
     const int K5=7;
@@ -29,12 +29,14 @@ SoftwareSerial mySerial(1, 0); // RX, TX
     
 //czujniki analogowe SA1...SA6 wpiętę do wejść analogowych arduino nr.: A0...A5const int SA1=A0; //Czujnik natężenia światła PT550
     const int SA1=A0; //Czujnik światła (fotorezystor)
-    const int SA2=A1;
+    const int SA2=A1; //Krańcówka
     const int SA3=A2;
     const int SA4=A3;
     const int SA5=A4;
     const int SA6=A5;
-    
+
+//zdefiniowanie zmiennej 'krańcówka' w której jest przetrzymowana informacja o tym czy brama otworzyła się do końca
+int limit_sensor=0;
 //zdefiniowanie zmiennej 'light_intensity', w której przetrzymywana jest informacja o natężeniu światła
     int light_intensity=0;
 
@@ -101,33 +103,45 @@ delay(100);
 
 void automatyka()
 {
+    //Na potrzeby debugowania
+    Serial1.println("Dziala automat");
     //Dokonanie odczytu z czujników, zapis wartości do zmiennych i włączenie odpowiednich cewek
 
-    //Światło
+    //Lampa
     light_intensity = analogRead(A0); //Odczyt natężenia światła z czujnika PT550 wpiętego do A0
 
-        if (light_intensity >= 700) //rozdzielczość na pinach analogowych w arduino wynosi 10 bitów, co znaczy że będziemy odbierać na nich liczby w zakresie <0, 1023> gdzie 0=0V a 1023=5V
-        {
-            digitalWrite(K1, HIGH);
-            Serial1.println("Dziala automat");
-        }
-        else
-        {
-            digitalWrite(K1, LOW);
-            Serial1.println("Dziala automat");
-        }
+     if (light_intensity < 700) //rozdzielczość na pinach analogowych w arduino wynosi 10 bitów, co znaczy że będziemy odbierać na nich liczby w zakresie <0, 1023> gdzie 0=0V a 1023=5V
+     {
+         digitalWrite(K1, HIGH);
+         Serial1.println("Zaświecono lampę");
+     }
+     else
+     {
+         digitalWrite(K1, LOW);
+         Serial1.println("Zgaszono lampę");   
+     }
         
-        delay(200);
-    //Temperatura i Wilgotność
-    //int wilgotnosc = dht.getHumidity(); //Pobranie informacji o wilgotnosci
-    //int temperatura = dht.getTemperature(); //Pobranie informacji o temperaturze
+     
+    //Klima
+   // int wilgotnosc = dht.getHumidity(); //Pobranie informacji o wilgotnosci
+   // int temperatura = dht.getTemperature(); //Pobranie informacji o temperaturze
 
-    //Odległość
+    //Brama
     unsigned int uS = sonar.ping(); //Odczyt z czujnika ultradźwiękowego HC-SR04  
     distance = uS / US_ROUNDTRIP_CM; //Przetworzenie dostarczonego sygnały na odległość w [cm]
-    
-    delay(1000);
+    limit_sensor = analogRead(A1);
 
+     if (distance < 10 && limit_sensor < 1000)
+     {
+         digitalWrite(K2, HIGH);
+         Serial1.println("Trwa otwieranie bramy");
+     }
+    else if(distance > 15 && limit_sensor > 1003) //Zmiana obrotów silnika!!!!!!!!!!!!!!
+     {   
+         digitalWrite(K2, LOW);
+         Serial1.println("UWAGA! Trwa zamykanie bramy")
+         miganie(K3);
+     }
 }
 
 //Funkcja ręcznego załączania cewek
@@ -163,4 +177,13 @@ void reczne()
     
     default: break;
   }
+}
+
+//Funkcja załącząjąca u wyłączająca napięcie na wyjściu co 700ms
+void miganie(int out) //"out" to parametr funkcji, który opisuje wyjście na którym działanie ma zostać przeprowadzone, np.: miganie(K3); Tak jak wyżej w funkcji automatyka();
+{
+    digitalWrite(out, HIGH);
+    delay(700);
+    digitalWrite(out, LOW);
+    delay(700);
 }
